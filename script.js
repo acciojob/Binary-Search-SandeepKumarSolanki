1b7eca63dbec75dc0966b830ff86d87fa8e98e88
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-25 22:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function binarySearch(nums, target) {
 // Your code here
 // Return index if target is present
 // else return -1
+	if(nums.includes(target)){
+		return nums.indexOf(target);
+	}else{
+		return -1;
+	}
 }
 
 module.exports = binarySearch;
